80db8696348a7b6b808e0058da69bde59a8e641e
Mariana <mariana.obm@gmail.com>
2025-04-21 12:10:04 +0100

    Stated working on Contact class

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,7 +6,21 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 19:58:57 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/18 19:58:58 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/04/21 11:42:58 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Contact.hpp"
+
+Contact::Contact(void)
+{
+	std::cout << "Constructor called" << std::endl;
+	return ;
+}
+
+Contact::~Contact(void)
+{
+	std::cout << "Descructor called" << std::endl;
+	return ;
+}
--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -6,7 +6,18 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 20:03:05 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/18 20:03:06 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/04/21 11:40:20 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+class Contact
+{
+	public:
+		Contact(void);
+		~Contact(void);
+};
+
+#endif //CONTACT_HPP
