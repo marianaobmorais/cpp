b6c250ea71defff0c687fc663e656d67b26b63af
Mariana <mariana.obm@gmail.com>
2025-04-21 12:09:11 +0100

    Added main

---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 00:43:56 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/04/21 12:07:43 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "PhoneBook.hpp"
+//#include "Contact.hpp"
+
+int	main(void)
+{
+	PhoneBook	phonebook; //intatiate is like declaring in C
+
+	phonebook.var = 42;
+	std::cout << "phonebook.var: " << phonebook.var << std::endl;
+	
+	phonebook.test();
+	
+	return (0);
+}
+
+
+// int	main(void)
+// {
+// 	char	buff[1024]; //check limit?
+
+// 	std::cout << "Welcome to My Awesome Phonebook!" << std::endl;
+// 	std::cout << "Please enter one of the three commands:" << std::endl;
+// 	std::cout << "<ADD> <SEARCH> <EXIT>: " << std::endl;
+// 	std::cin >> buff;
+// 	//while (buff.compare("EXIT") != 0)
+// 	// {
+// 	// 	std::cin >> buff;
+// 	// }
+// 	std::cout << "You entered: [" << buff << "]" << std::endl;
+// 	return (0);
+// }
