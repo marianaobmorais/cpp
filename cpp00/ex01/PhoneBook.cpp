daa395e7b83b81f2f097ca4c6396f53af8cf7ddf
Mariana <mariana.obm@gmail.com>
2025-04-21 12:10:24 +0100

    Working on PhoneBook class

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,7 +6,28 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 19:58:47 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/18 19:58:48 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/04/21 10:50:57 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "PhoneBook.hpp"
+
+//constructor and destructor don't have a return type
+PhoneBook::PhoneBook(void)
+{
+	std::cout << "Constructor called" << std::endl;
+	return ;
+}
+
+PhoneBook::~PhoneBook(void)
+{
+	std::cout << "Destructor called"<< std::endl;
+	return ;
+}
+
+void	PhoneBook::test(void)
+{
+	std::cout << "Member function 'test' called" << std::endl;
+	return ;
+}
--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,7 +6,22 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 19:59:30 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/18 19:59:31 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/04/21 10:49:19 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+class PhoneBook
+{
+	public:
+		PhoneBook(void);
+		~PhoneBook(void);
+
+		int	var;
+
+		void	test(void);
+};
+
+#endif //PHONEBOOK_HPP
