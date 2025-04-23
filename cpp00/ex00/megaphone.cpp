0baa3341d69b2c7086c184a5c244740b2b5fc260
Mariana <mariana.obm@gmail.com>
2025-04-23 13:57:59 +0100

    Updated arg.size to arg.length to be more semantically meaningful

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 16:24:16 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/18 19:05:40 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/04/23 13:55:34 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int argc, char **argv)
 		{
 			std::string arg(argv[i]);
 			std::string::size_type j = 0;
-			while (j < arg.size())
+			while (j < arg.length())
 			{
 				std::cout << static_cast<char>(std::toupper(arg[j]));
 				j++;
