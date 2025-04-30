e8434b15fbfdbf57ae46889633eeb5b73628ac50
Mariana <mariana.obm@gmail.com>
2025-04-30 18:33:29 +0100

    Added comments explaining functions

--- cpp00/ex01/utils.cpp
+++ cpp00/ex01/utils.cpp
@@ -6,12 +6,15 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 14:02:16 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/23 15:21:29 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/04/30 18:30:46 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.hpp"
 
+/// @brief Checks if a string contains only digits.
+/// @param str The string to check.
+/// @return True if the string contains only digits, false otherwise.
 bool	all_digits(const std::string &str)
 {
 	std::string::size_type	i;
@@ -28,6 +31,9 @@ bool	all_digits(const std::string &str)
 	return (true);
 }
 
+/// @brief Truncates a string to a maximum length of 10 characters.
+/// @param str The string to truncate.
+/// @return The truncated string, ending with a dot (.) if it exceeds 10 characters, or the original string if it doesn't.
 std::string	truncate(const std::string &str)
 {
 	if (str.length() > 10)
