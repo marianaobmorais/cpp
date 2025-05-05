e446cbd42119012eb4a2a3cc13999a7da5bffeb1
Mariana <mariana.obm@gmail.com>
2025-05-05 18:10:18 -0300

    Created main

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,17 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:36:30 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/30 19:36:31 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/05 22:03:02 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.h"
+
+int	main(void)
+{
+	Zombie	*z = newZombie("Robson");
+	z->annouce();
+	delete z;
+	randomChump("Foo");
+	return (0);
+}
