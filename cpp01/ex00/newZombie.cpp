8d6f38e89a0d0337e9f63e59354d6a79cfdc34d2
Mariana <mariana.obm@gmail.com>
2025-05-05 18:10:50 -0300

    Created newZombie function

--- cpp01/ex00/newZombie.cpp
+++ cpp01/ex00/newZombie.cpp
@@ -6,7 +6,13 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:37:15 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/30 19:37:17 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/05 21:16:20 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+Zombie*	newZombie(std::string name)
+{
+	return (new Zombie(name));
+}
