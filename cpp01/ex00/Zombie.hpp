8bebed54b0444b5084b29869a44246dd5690e588
Mariana <mariana.obm@gmail.com>
2025-05-06 13:08:34 -0300

    Created Zombie.hpp

--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -6,7 +6,23 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:36:45 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/30 19:36:46 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/05 22:06:39 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+class Zombie
+{
+	private:
+		std::string	name;
+	public:
+		Zombie(std::string name);
+		~Zombie(void);
+		void	annouce(void);
+};
+
+#endif //ZOMBIE_HPP;
