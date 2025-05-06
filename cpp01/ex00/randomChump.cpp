9f1d4b63cd0255dce5a0eb55d2d2ba8f41ae533c
Mariana <mariana.obm@gmail.com>
2025-05-06 13:09:32 -0300

    Created randomChump.cpp

--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -6,7 +6,14 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:37:36 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/30 19:37:37 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/05 21:16:09 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.h"
+
+void	randomChump(std::string name)
+{
+	Zombie	zombie(name); //constructor with parameter
+	zombie.annouce();
+}
