7ee2f1c825a97b9cc71e2ce2826f9cd6f83c52b3
Mariana <mariana.obm@gmail.com>
2025-05-06 13:08:08 -0300

    Created Zombie.cpp

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -6,7 +6,20 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:37:00 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/04/30 19:37:01 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/05 22:06:43 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string newName) : name(newName){}
+
+Zombie::~Zombie(void)
+{
+	std::cout << "Destructor message: " << name << " was destroyed" << std::endl;
+}
+
+void	Zombie::annouce(void)
+{
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
