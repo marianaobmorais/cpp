23da69297fd1de34c76f7afc4bb1451fdac4b220
Mariana <mariana.obm@gmail.com>
2025-05-06 15:38:49 -0300

    Created zombieHorde function

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,12 +6,23 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 17:26:04 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/05/06 17:28:10 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/06 19:34:58 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+#include <sstream>
 
 Zombie*	zombieHorde(int N, std::string name)
 {
+	if (N <= 0)
+		return (NULL);
+	Zombie	*horde = new Zombie[N];
+	for (int i = 0; i < N; i++)
+	{
+		std::ostringstream oss;
+		oss << (i + 1);
+		horde[i].setName(name + oss.str());
+	}
+	return (horde);
 }
