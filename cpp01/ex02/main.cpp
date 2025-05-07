d84626bf8cbd8694533e922944cdeb5b5a76fd94
Mariana <mariana.obm@gmail.com>
2025-05-07 11:20:23 -0300

    Added main body

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,11 +6,24 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:07:37 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/05/06 20:07:50 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/07 15:16:37 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
 int	main(void)
 {
+	std::string		str = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
+
+	std::cout << "Memory address of str: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
 	return (0);
 }
