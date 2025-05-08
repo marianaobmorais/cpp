87c71e6937661ef4dd644806d07580effadcec5c
Mariana <mariana.obm@gmail.com>
2025-05-08 10:16:48 -0300

    Created Weapon.hpp

---
+++ cpp01/ex03/Weapon.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 15:29:55 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/05/07 15:32:02 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+#include <string>
+
+class Weapon
+{
+	private:
+		std::string	type;
+	public:
+		Weapon(void);
+		~Weapon(void);
+		const std::string getType();
+};
+
+#endif //WEAPON_HPP
