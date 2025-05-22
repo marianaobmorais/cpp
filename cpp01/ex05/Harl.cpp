9139fb8f4f69ba8eb74bcdd5714359e1b7f6eace
mariaoli <mariaoli@student.42porto.com>
2025-05-22 15:29:16 +0100

    Updated functions

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 00:14:00 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/05/17 02:21:15 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/22 15:28:37 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,20 +35,24 @@ void	Harl::complain(std::string level)
 
 void	Harl::debug(void)
 {
+	std::cout << "[DEBUG]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
 }
 
 void	Harl::info(void)
 {
+	std::cout << "[INFO]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void	Harl::warning(void)
 {
+	std::cout << "[WARNING]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
 }
 
 void	Harl::error(void)
 {
+	std::cout << "[ERROR]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 00:13:27 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/05/17 02:17:54 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/22 15:28:32 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,9 @@ int	main(void)
 {
 	Harl	harl;
 
-	std::cout << "DEBUG:" << std::endl;
 	harl.complain("DEBUG");
-	std::cout << "INFO:" << std::endl;
 	harl.complain("INFO");
-	std::cout << "WARNING:" << std::endl;
 	harl.complain("WARNING");
-	std::cout << "ERROR:" << std::endl;
 	harl.complain("ERROR");
 	return (0);
 }
