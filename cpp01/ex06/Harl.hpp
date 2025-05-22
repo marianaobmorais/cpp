36b3daef608abd5e3e533bfb3ae48b89d5cb4722
mariaoli <mariaoli@student.42porto.com>
2025-05-22 20:53:52 +0100

    Created Harl.hpp

---
+++ cpp01/ex06/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 15:41:59 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/05/22 15:45:17 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		Harl(void);
+		~Harl(void);
+		void	complain(std::string level);
+};
+
+#endif //HARL_HPP
