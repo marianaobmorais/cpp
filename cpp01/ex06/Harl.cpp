f44119835b9dcfb9a969a74b197cabc3c6c5ac40
mariaoli <mariaoli@student.42porto.com>
2025-05-26 17:03:35 +0100

    Built Harl::complain() function

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 15:45:46 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/05/22 16:01:24 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/26 16:59:17 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,36 @@ Harl::~Harl(void) {}
 
 void	Harl::complain(std::string level)
 {
-	
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int			i = 0;
+	int			index = -1;
+
+	while (i < 4)
+	{
+		if (levels[i] == level)
+		{
+			index = i;
+			break ;
+		}
+		i++;
+	}
+	switch (index)
+	{
+		case 0:
+			this->debug();
+			break ;
+		case 1:
+			this->info();
+			break ;
+		case 2:
+			this->warning();
+			break ;
+		case 3:
+			this->error();
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 }
 
 void	Harl::debug(void)
