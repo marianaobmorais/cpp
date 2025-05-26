79601a39232907694371f4b2ac6fb736aa390ab3
mariaoli <mariaoli@student.42porto.com>
2025-05-26 17:04:13 +0100

    Refactored main.cpp

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 15:41:19 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/05/22 16:12:26 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/05/26 17:00:40 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,13 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc == 1)
-	{
-		std::cout << "USAGE: ./harlFilter <level>" << std::endl;
-		return (1);
-	}
-	if (argc == 2)
+	if (argc != 2)
 		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	else
 	{
 		Harl		harl;
 		std::string	str(argv[1]);
-
+		
 		harl.complain(str);
 	}
 	return (0);
