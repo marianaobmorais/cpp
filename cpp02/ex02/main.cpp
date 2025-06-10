f86bde765cb7e6da13d6f9921fd65f7ca183bf42
Mariana <mariana.obm@gmail.com>
2025-06-10 23:44:25 +0100

    Created main.cpp

---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/31 00:50:20 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/06/10 23:28:59 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed		a;
+	Fixed const	b(Fixed( 5.05f ) * Fixed( 2 ));
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return (0);
+}
