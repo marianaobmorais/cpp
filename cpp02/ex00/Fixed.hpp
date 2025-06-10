8a2be5a5d6ec75cc445c794bd803f6da83f6d1a9
Mariana <mariana.obm@gmail.com>
2025-06-10 16:59:57 +0100

    Updated Fixed.hpp

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 00:51:29 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/07 11:15:52 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/09 16:10:35 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 class Fixed
 {
 	private:
-		int					fixedPointValue;
-		static const int	fractionalBits;
+		int					_fixedPointValue;
+		static const int	_fractionalBits = 8;
 	public:
-		Fixed(void);							//canonical
-		~Fixed(void);							//canonical
-		Fixed(Fixed const& src);				//canonical
-		Fixed&	operator=(Fixed const& rhs);	//canonical
+		Fixed(void);							//canonical form //default constructor
+		Fixed(Fixed const& src);				//canonical form //copy constructor //creates new instance
+		Fixed&	operator=(Fixed const& rhs);	//canonical form //copy assignment operator //updates the current instance
+		~Fixed(void);							//canonical form //destructor
 
-		int		getRawBits(void);
+		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 };
 
