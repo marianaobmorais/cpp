1132f1f5f99f85feedbeb3e2544e6bedc05fa440
Mariana <mariana.obm@gmail.com>
2025-06-10 20:47:39 +0100

    Created ex01/main.cpp

---
+++ cpp02/ex01/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/31 00:50:20 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/06/10 17:07:21 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int	main( void )
+{
+	Fixed a;
+	Fixed const b( 10 ); Fixed const c( 42.42f ); Fixed const d( b );
+
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return (0);
+}
