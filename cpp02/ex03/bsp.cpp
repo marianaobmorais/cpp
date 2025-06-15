25969762f97b948bf47401d8c0f8672df4706676
Mariana Oliveira Braga De Morais <mariaoli@c2r6s10.42porto.com>
2025-06-15 19:03:31 +0100

    Added briefs and more checks for safety

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,12 +6,17 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:57:08 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/11 18:25:57 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/15 18:58:23 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
+/// @brief Computes the absolute value of the area of a triangle using the determinant method.
+/// @param a First vertex of the triangle.
+/// @param b Second vertex of the triangle.
+/// @param c Third vertex of the triangle.
+/// @return The area as a Fixed-point number.
 Fixed	area(Point const& a, Point const& b, Point const& c)
 {
 	Fixed	term1 = a.getX() * (b.getY() - c.getY());
@@ -19,11 +24,17 @@ Fixed	area(Point const& a, Point const& b, Point const& c)
 	Fixed	term3 = c.getX() *(a.getY() - b.getY());
 	Fixed	area = term1 + term2 + term3;
 
-	if (area < 0)
-		area = area * -1;
-	return (area/Fixed(2));
+	if (area < Fixed(0))
+		area = area * Fixed(-1);
+	return (area / Fixed(2));
 }
 
+/// @brief Checks if a point lies inside a triangle using area comparison.
+/// @param a First vertex of the triangle.
+/// @param b Second vertex of the triangle.
+/// @param c Third vertex of the triangle.
+/// @param point The point to check.
+/// @return true if the point lies strictly inside the triangle (not on the edge).
 bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
 	Fixed	totalArea = area(a, b, c);
@@ -32,5 +43,7 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point)
 	Fixed	areaABP = area(a, b, point);
 	Fixed	sum = areaPBC + areaAPC + areaABP;
 
-	return (totalArea == sum);
+	if (areaPBC != Fixed(0) && areaAPC != Fixed(0) && areaABP != Fixed(0) && totalArea == sum)
+		return (true);
+	return (false);
 }
