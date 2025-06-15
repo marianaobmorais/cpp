5f391dcb3919c51fb6da5eb5624f65afdc1c8e81
Mariana Oliveira Braga De Morais <mariaoli@c2r6s10.42porto.com>
2025-06-15 19:06:06 +0100

    Refactored assignment operator overload and added briefs

--- cpp02/ex03/Point.cpp
+++ cpp02/ex03/Point.cpp
@@ -6,36 +6,46 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:38:15 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/11 18:49:30 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/15 18:56:19 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-Point::Point(void) : _x(0), _y(0) {}
+/// @brief Default constructor. Initializes the point at coordinates (0, 0).
+Point::Point(void) : x(0), y(0) {}
 
-Point::Point(float const x, float const y) : _x(x), _y(y) {}
+/// @brief Constructs a point with the given x and y coordinates.
+/// @param x The x-coordinate.
+/// @param y The y-coordinate.
+Point::Point(float const x, float const y) : x(x), y(y) {}
 
-Point::Point(Point const& src) : _x(src._x), _y(src._y) {}
+/// @brief Copy constructor. Creates a copy of the given Point.
+/// @param src The Point to copy.
+Point::Point(Point const& src) : x(src.x), y(src.y) {}
 
+/// @brief Copy assignment operator. No-op due to const attributes.
+/// @param rhs The Point to assign from.
+/// @return Reference to this Point.
 Point&	Point::operator=(Point const& rhs)
 {
-	if (this != &rhs)
-	{
-		this->_x = rhs._x;
-		this->_y = rhs._y;
-	}
+	(void)rhs;
 	return (*this);
 }
 
+/// @brief Returns the x-coordinate of the point.
+/// @return A constant reference to the x-coordinate.
 Fixed const&	Point::getX(void) const
 {
-	return (_x);
+	return (x);
 }
 
+/// @brief Returns the y-coordinate of the point.
+/// @return A constant reference to the y-coordinate.
 Fixed const&	Point::getY(void) const
 {
-	return (_y);
+	return (y);
 }
 
+/// @brief Destructor. Destroys the Point instance.
 Point::~Point(void) {}
