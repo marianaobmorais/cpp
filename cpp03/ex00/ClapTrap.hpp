c9100ec5b0e37e6053635793fd7888cbd57464aa
Mariana Oliveira Braga De Morais <mariaoli@c2r6s10.42porto.com>
2025-06-16 20:30:59 +0100

    Commented out accessor functions

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 09:56:38 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/16 10:50:16 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/16 20:29:38 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,11 @@ class ClapTrap
 		void			takeDamage(unsigned int amount);
 		void			beRepaired(unsigned int amount);
 
-		std::string		getName(void) const;
-		unsigned int	getHitPoints(void) const;
-		unsigned int	getEnergyPoints(void) const;
-		unsigned int	getAttackDamage(void) const;
-		void			setAttackDamage(unsigned int damage);
+		// std::string		getName(void) const;
+		// unsigned int	getHitPoints(void) const;
+		// unsigned int	getEnergyPoints(void) const;
+		// unsigned int	getAttackDamage(void) const;
+		// void			setAttackDamage(unsigned int damage);
 };
 
 #endif //CLAPTRAP_HPP
