8c4eb127fa98e2bd796b0093d81c2f8b3587ddbf
Mariana <mariana.obm@gmail.com>
2025-06-17 19:52:47 +0100

    Added ex01/ScavTrap.cpp

---
+++ cpp03/ex01/ScavTrap.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 19:36:17 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/06/17 19:49:18 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(void)
+{
+	
+}
+
+ScavTrap::ScavTrap(ScavTrap const& src)
+{
+	
+}
+
+ScavTrap&	ScavTrap::operator=(const ScavTrap& rhs)
+{
+	
+}
+
+ScavTrap::~ScavTrap(void)
+{
+	
+}
+
+void	ScavTrap::attack(const std::string& target)
+{
+	
+}
+
+void	ScavTrap::guardGate(void)
+{
+	
+}
