a0c836cc9a06296ddc074012603ca01cd96c4a4b
Mariana <mariana.obm@gmail.com>
2025-06-17 10:03:01 +0100

    Improved functions logic and re-added getter functions

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,19 +6,19 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 10:07:23 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/16 20:30:05 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/17 10:02:08 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap(void) :
-	name("Default name"),
+	name("Default Name"),
 	hitPoints(10),
 	energyPoints(10),
 	attackDamage(0)
 {
-	std::cout << "Default constructor called" <<std::endl;
+	std::cout << "Default constructor called for " << this->name << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string newName) :
@@ -50,71 +50,60 @@ ClapTrap&	ClapTrap::operator=(const ClapTrap& rhs)
 
 ClapTrap::~ClapTrap(void)
 {
-	//using accessor function here is redundant?
 	std::cout << "Destructor called for " << this->name << std::endl;
 }
 
 void	ClapTrap::attack(const std::string& target)
 {
-	if (energyPoints > 0 && hitPoints > 0)
+	if (this->energyPoints <= 0 && this->hitPoints <= 0)
+		std::cout << "ClapTrap " << this->name << " cannot attack. Not enough energy points nor hit points." << std::endl;
+	else if (this->energyPoints <= 0)
+		std::cout << "ClapTrap " << this->name << " cannot attack. Not enough energy point." << std::endl;
+	else if (this->hitPoints <= 0)
+		std::cout << "ClapTrap " << this->name << " cannot attack. Not enough hit points." << std::endl;
+	else
 	{
-		//using accessor function here is redundant?
 		std::cout << "ClapTrap " << this->name << " attacks " << target << ", causing " << this->attackDamage << " points of damage!" << std::endl;
-		energyPoints--;
+		this->energyPoints--;
 	}
-	else if (energyPoints <= 0)
-		std::cout << "ClapTrap cannot attack. Not enough energy points." << std::endl;
-	else if (hitPoints <= 0)
-		std::cout << "ClapTrap cannot attack. Not enough hit points." << std::endl;
 }
 
 void	ClapTrap::takeDamage(unsigned int amount)
 {
-	//using accessor function here is redundant?
 	std::cout << "ClapTrap " << this->name << " took " << amount << " points of damage!" << std::endl;
-	if (hitPoints >= amount)
-		hitPoints -= amount;
+	if (this->hitPoints >= amount)
+		this->hitPoints -= amount;
 	else
-		hitPoints = 0;
-
+		this->hitPoints = 0;
 }
 
 void	ClapTrap::beRepaired(unsigned int amount)
 {
-	if (energyPoints > 0 && hitPoints > 0)
+	if (this->energyPoints <= 0 && this->hitPoints <= 0)
+		std::cout << "ClapTrap " << this->name << " cannot attack. Not enough energy points nor hit points." << std::endl;
+	else if (this->energyPoints <= 0)
+		std::cout << "ClapTrap " << this->name << " cannot repair itself. Not enough energy points." << std::endl;
+	else if (this->hitPoints <= 0)
+		std::cout << "ClapTrap " << this->name << " cannot repair itself. Not enough hit points." << std::endl;
+	else
 	{
-		//using accessor function here is redundant?
 		std::cout << "ClapTrap " << this->name << " repairs itself with " << amount << " points of hit!" << std::endl;
-		energyPoints--;
-		hitPoints += amount;
+		this->energyPoints--;
+		this->hitPoints += amount;
 	}
-	else if (energyPoints <= 0)
-		std::cout << "ClapTrap cannot repair itself. Not enough energy points." << std::endl;
-	else if (hitPoints <= 0)
-		std::cout << "ClapTrap cannot repair itself. Not enough hit points." << std::endl;
 }
 
-// std::string	ClapTrap::getName(void) const
-// {
-// 	return (this->name);
-// }
+std::string	ClapTrap::getName(void) const
+{
+	return (this->name);
+}
 
-// unsigned int	ClapTrap::getHitPoints(void) const
-// {
-// 	return (this->hitPoints);
-// }
+unsigned int	ClapTrap::getHitPoints(void) const
+{
+	return (this->hitPoints);
+}
 
-// unsigned int	ClapTrap::getEnergyPoints(void) const
-// {
-// 	return (this->energyPoints);
-// }
-
-// unsigned int	ClapTrap::getAttackDamage(void) const
-// {
-// 	return (this->attackDamage);
-// }
-
-// void	ClapTrap::setAttackDamage(unsigned int damage)
-// {
-// 	this->attackDamage = damage;
-// }
+unsigned int	ClapTrap::getEnergyPoints(void) const
+{
+	return (this->energyPoints);
+}
