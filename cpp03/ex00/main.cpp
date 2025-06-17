c6ae02570d49a47c59979865332df976899b2215
Mariana <mariana.obm@gmail.com>
2025-06-17 10:03:26 +0100

    Added more tests to main

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 19:53:46 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/16 20:20:42 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/17 09:54:24 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,38 @@
 
 int	main(void)
 {
-	ClapTrap	c("Carlos");
+	ClapTrap	a;
+	ClapTrap	b("Carlos");
+	ClapTrap	c(b);
+	ClapTrap	d;
+
+	d = b;
 	
-	c.attack("banana");
-	c.beRepaired(10);
-	c.attack("abacaxi");
-	c.attack("melancia");
+	std::cout << "----Attack----" << std::endl;
+	b.attack("Target1");
+	std::cout << b.getName() << " now has " << b.getEnergyPoints() << " energy points left" << std::endl;
+	
+	std::cout << "----Take damage----" << std::endl;
+	b.takeDamage(6);
+	std::cout << b.getName() << " now has " << b.getHitPoints() << " hit points left" << std::endl;
+	std::cout << b.getName() << " still has " << b.getEnergyPoints() << " energy points left" << std::endl;
+	
+	std::cout << "----Be repaired----"<< std::endl;
+	b.beRepaired(4);
+	std::cout << b.getName() << " now has " << b.getHitPoints() << " hit points left" << std::endl;
+	std::cout << b.getName() << " now has " << b.getEnergyPoints() << " energy points left" << std::endl;
+
+	std::cout << "----Use all energy points----" << std::endl;
+	while (b.getEnergyPoints())
+	{
+		b.attack("Target2");
+		std::cout << b.getName() << " now has " << b.getEnergyPoints() << " energy points left" << std::endl;
+	}
+
+	std::cout << "----Take more damage----" << std::endl;
+	b.takeDamage(100);
+	b.attack("Target3");
+	b.beRepaired(10);
 	
 	return (0);
 }
