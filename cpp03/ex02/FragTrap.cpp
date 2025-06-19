20631f9dd453a80c016f7491cdf7889cc4c65688
Mariana Morais <mariaoli@student.42porto.com>
2025-06-19 20:24:03 +0100

    Added briefs

--- cpp03/ex02/FragTrap.cpp
+++ cpp03/ex02/FragTrap.cpp
@@ -6,12 +6,14 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 09:31:10 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/19 11:59:28 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/19 20:09:17 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 
+/// @brief Default constructor for FragTrap.
+/// Initializes with default name and specific hit/energy/attack points.
 FragTrap::FragTrap(void) : ClapTrap("Default FragTrap")
 {
 	this->hitPoints = 100;
@@ -20,6 +22,8 @@ FragTrap::FragTrap(void) : ClapTrap("Default FragTrap")
 	std::cout << "FragTrap default constructor called for " << this->name << std::endl;
 }
 
+/// @brief Parameterized constructor for FragTrap.
+/// @param newName The name to assign to the FragTrap.
 FragTrap::FragTrap(std::string const& newName) : ClapTrap(newName)
 {
 	this->hitPoints = 100;
@@ -28,11 +32,17 @@ FragTrap::FragTrap(std::string const& newName) : ClapTrap(newName)
 	std::cout << "FragTrap parameterized constructor called for " << this->name << std::endl;
 }
 
+/// @brief Copy constructor for FragTrap.
+/// @param src The FragTrap to copy from.
 FragTrap::FragTrap(FragTrap const& src) : ClapTrap(src)
 {
 	std::cout << "FragTrap copy constructor called" << std::endl;
 }
 
+
+/// @brief Copy assignment operator for FragTrap.
+/// @param rhs The FragTrap to assign from.
+/// @return Reference to the updated object.
 FragTrap&	FragTrap::operator=(FragTrap const& rhs)
 {
 	if (this != &rhs)
@@ -41,11 +51,14 @@ FragTrap&	FragTrap::operator=(FragTrap const& rhs)
 	return (*this);
 }
 
+/// @brief Destructor for FragTrap.
 FragTrap::~FragTrap(void)
 {
 	std::cout << "FragTrap destructor called for " << this->name << std::endl;
 }
 
+
+/// @brief Prints a positive high-fives request message.
 void	FragTrap::highFivesGuys(void)
 {
 	std::cout << "This is a positive high-fives request :-)" << std::endl;
