941d9cc289ca17ed266d2e2affec392b3c6ab7e0
Mariana <mariana.obm@gmail.com>
2025-06-19 09:23:44 +0100

    Added line breaks for better readability

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 19:53:46 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/18 19:56:38 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/19 09:22:11 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 
 int main(void)
 {
-	std::cout << "------------Construct ClapTrap------------" << std::endl;
+	std::cout << "\n------------Construct ClapTrap------------" << std::endl;
 	ClapTrap c("Carlos");
+	std::cout << c.getName() << " has " << c.getHitPoints() << " hit points and " << c.getEnergyPoints() << " energy points" << std::endl;
 	c.attack("Target1");
 	c.takeDamage(3);
 	c.beRepaired(5);
 	std::cout << c.getName() << " now has " << c.getHitPoints() << " hit points and " << c.getEnergyPoints() << " energy points" << std::endl;
 
-	std::cout << "------------Construct ScavTrap------------" << std::endl;
+	std::cout << "\n------------Construct ScavTrap------------" << std::endl;
 	ScavTrap s1("Matheus");
 	std::cout << s1.getName() << " has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
 	
-	std::cout << "------------Actions------------" << std::endl;
+	std::cout << "\n------------Actions------------" << std::endl;
 	std::cout << "*Base class function overwritten*" << std::endl;
 	s1.attack("target2");
 	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
@@ -38,13 +39,13 @@ int main(void)
 	s1.beRepaired(10);
 	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
 
-	std::cout << "------------Copy & Assignment------------" << std::endl;
+	std::cout << "\n------------Copy & Assignment------------" << std::endl;
 	ScavTrap s2(s1);
 	ScavTrap s3;
 	s3 = s1;
 	std::cout << s3.getName() << " (copy) now has " << s3.getHitPoints() << " hit points and " << s3.getEnergyPoints() << " energy points" << std::endl;
 
 
-	std::cout << "------------Destruct------------" << std::endl;
+	std::cout << "\n------------Destruct------------" << std::endl;
 	return (0);
 }
