44c394e85a68b70a23b9f1b9832fb866aa0b5eb6
Mariana <mariana.obm@gmail.com>
2025-06-19 11:26:22 +0100

    Completed tests for main

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -6,45 +6,75 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 19:53:46 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/19 09:22:11 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/19 11:18:41 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
-#include "ScavTrap.hpp"
+// #include "ClapTrap.hpp"
+// #include "ScavTrap.hpp"
+
+// int main(void)
+// {
+// 	std::cout << "\n------------Construct ClapTrap------------" << std::endl;
+// 	ClapTrap c("Carlos");
+// 	std::cout << c.getName() << " has " << c.getHitPoints() << " hit points and " << c.getEnergyPoints() << " energy points" << std::endl;
+// 	c.attack("Target1");
+// 	c.takeDamage(3);
+// 	c.beRepaired(5);
+// 	std::cout << c.getName() << " now has " << c.getHitPoints() << " hit points and " << c.getEnergyPoints() << " energy points" << std::endl;
+
+// 	std::cout << "\n------------Construct ScavTrap------------" << std::endl;
+// 	ScavTrap s1("Matheus");
+// 	std::cout << s1.getName() << " has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
+	
+// 	std::cout << "\n------------Actions------------" << std::endl;
+// 	std::cout << "*Base class function overwritten*" << std::endl;
+// 	s1.attack("target2");
+// 	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
+// 	std::cout << "*Derived class new function*" << std::endl;
+// 	s1.guardGate();
+// 	std::cout << "*Base class function not overwritten*" << std::endl;
+// 	s1.takeDamage(90);
+// 	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
+// 	s1.beRepaired(10);
+// 	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
+
+// 	std::cout << "\n------------Copy & Assignment------------" << std::endl;
+// 	ScavTrap s2(s1);
+// 	ScavTrap s3;
+// 	s3 = s1;
+// 	std::cout << s3.getName() << " (copy) now has " << s3.getHitPoints() << " hit points and " << s3.getEnergyPoints() << " energy points" << std::endl;
+
+
+// 	std::cout << "\n------------Destruct------------" << std::endl;
+// 	return (0);
+// }
+
+#include "FragTrap.hpp"
 
 int main(void)
 {
-	std::cout << "\n------------Construct ClapTrap------------" << std::endl;
-	ClapTrap c("Carlos");
-	std::cout << c.getName() << " has " << c.getHitPoints() << " hit points and " << c.getEnergyPoints() << " energy points" << std::endl;
-	c.attack("Target1");
-	c.takeDamage(3);
-	c.beRepaired(5);
-	std::cout << c.getName() << " now has " << c.getHitPoints() << " hit points and " << c.getEnergyPoints() << " energy points" << std::endl;
-
-	std::cout << "\n------------Construct ScavTrap------------" << std::endl;
-	ScavTrap s1("Matheus");
-	std::cout << s1.getName() << " has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
+	std::cout << "\n------------Construct FragTrap------------" << std::endl;
+	FragTrap f1("Luciano");
+	std::cout << f1.getName() << " (original) has " << f1.getHitPoints() << " hit points and " << f1.getEnergyPoints() << " energy points" << std::endl;
 	
 	std::cout << "\n------------Actions------------" << std::endl;
-	std::cout << "*Base class function overwritten*" << std::endl;
-	s1.attack("target2");
-	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
 	std::cout << "*Derived class new function*" << std::endl;
-	s1.guardGate();
+	f1.highFivesGuys();
 	std::cout << "*Base class function not overwritten*" << std::endl;
-	s1.takeDamage(90);
-	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
-	s1.beRepaired(10);
-	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
+	f1.attack("Target");
+	std::cout << f1.getName() << " now has " << f1.getHitPoints() << " hit points and " << f1.getEnergyPoints() << " energy points" << std::endl;
+	f1.takeDamage(90);
+	std::cout << f1.getName() << " now has " << f1.getHitPoints() << " hit points and " << f1.getEnergyPoints() << " energy points" << std::endl;
+	f1.beRepaired(10);
+	std::cout << f1.getName() << " now has " << f1.getHitPoints() << " hit points and " << f1.getEnergyPoints() << " energy points" << std::endl;
 
 	std::cout << "\n------------Copy & Assignment------------" << std::endl;
-	ScavTrap s2(s1);
-	ScavTrap s3;
-	s3 = s1;
-	std::cout << s3.getName() << " (copy) now has " << s3.getHitPoints() << " hit points and " << s3.getEnergyPoints() << " energy points" << std::endl;
-
+	FragTrap f2(f1);
+	std::cout << f2.getName() << " (copy) has " << f2.getHitPoints() << " hit points and " << f2.getEnergyPoints() << " energy points" << std::endl;
+	FragTrap f3;
+	f3 = f1;
+	std::cout << f3.getName() << " (assigned) has " << f3.getHitPoints() << " hit points and " << f3.getEnergyPoints() << " energy points" << std::endl;
 
 	std::cout << "\n------------Destruct------------" << std::endl;
 	return (0);
