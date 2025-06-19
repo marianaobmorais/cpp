ad816304d434c51499d504b93e4b81ebf446b334
Mariana <mariana.obm@gmail.com>
2025-06-19 11:25:18 +0100

    Added extra text to avoid ambiguity

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 19:53:46 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/19 09:22:11 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/19 11:21:38 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,26 +25,26 @@ int main(void)
 
 	std::cout << "\n------------Construct ScavTrap------------" << std::endl;
 	ScavTrap s1("Matheus");
-	std::cout << s1.getName() << " has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
+	std::cout << s1.getName() << " (original) has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
 	
 	std::cout << "\n------------Actions------------" << std::endl;
 	std::cout << "*Base class function overwritten*" << std::endl;
 	s1.attack("target2");
-	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
+	std::cout << s1.getName() << " (original) now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
 	std::cout << "*Derived class new function*" << std::endl;
 	s1.guardGate();
 	std::cout << "*Base class function not overwritten*" << std::endl;
 	s1.takeDamage(90);
-	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
+	std::cout << s1.getName() << " (original) now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
 	s1.beRepaired(10);
-	std::cout << s1.getName() << " now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
+	std::cout << s1.getName() << " (original) now has " << s1.getHitPoints() << " hit points and " << s1.getEnergyPoints() << " energy points" << std::endl;
 
 	std::cout << "\n------------Copy & Assignment------------" << std::endl;
 	ScavTrap s2(s1);
+	std::cout << s2.getName() << " (copy) has " << s2.getHitPoints() << " hit points and " << s2.getEnergyPoints() << " energy points" << std::endl;
 	ScavTrap s3;
 	s3 = s1;
-	std::cout << s3.getName() << " (copy) now has " << s3.getHitPoints() << " hit points and " << s3.getEnergyPoints() << " energy points" << std::endl;
-
+	std::cout << s3.getName() << " (assigned) has " << s3.getHitPoints() << " hit points and " << s3.getEnergyPoints() << " energy points" << std::endl;
 
 	std::cout << "\n------------Destruct------------" << std::endl;
 	return (0);
