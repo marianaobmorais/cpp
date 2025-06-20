0476dd45eb6dad9188f165967ef66a573694f87b
Mariana Morais <mariaoli@student.42porto.com>
2025-06-20 18:31:32 +0100

    Created Dog.hpp

---
+++ cpp04/ex00/Dog.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/20 18:20:58 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/06/20 18:26:07 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+# define DOG_HPP
+
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+	public:
+		Dog(void);
+		Dog(std::string &str);
+		Dog(Dog const& src);
+		Dog&	operator=(Dog const& rhs);
+		~Dog(void);
+};
+
+#endif //DOG_HPP
