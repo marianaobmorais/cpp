b42d246c884a13386be04bce39942ef085f4dfa6
Mariana <mariana.obm@gmail.com>
2025-06-20 23:33:15 +0100

    Created ex00/Cat.cpp

---
+++ cpp04/ex00/Cat.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/20 23:25:24 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/06/20 23:32:49 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+
+Cat::Cat(void)
+{
+	this->type = "Cat";
+	std::cout << "Cat default constructor called" << std::endl;
+}
+
+Cat::Cat(Cat const& src) : Animal(src)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+}
+
+Cat&	Cat::operator=(Cat const& rhs)
+{
+	if (this != &rhs)
+		Animal::operator=(rhs);
+	std::cout << "Cat copy assignment called" << std::endl;
+	return (*this);
+}
+
+Cat::~Cat(void)
+{
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+void	Cat::makeSound(void)
+{
+	std::cout << "Cat says: meow meow" << std::endl;
+}
