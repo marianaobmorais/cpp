9bb80509108817d914a029a34e317b19b1d30751
Mariana <mariana.obm@gmail.com>
2025-06-20 23:23:04 +0100

    Working on cpp04/ex00/main.cpp

---
+++ cpp04/ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/20 21:01:51 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/06/20 21:11:50 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int	main(void) 
+{
+	Animal const*	meta = new Animal();
+	Animal const*	j = new Dog();
+	Animal const*	i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound! j->makeSound();
+	meta->makeSound();
+	//...
+	return (0);
+}
