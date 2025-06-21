65215220ad51c33c4d02b679750a72f8a6750ec8
Mariana Morais <mariaoli@student.42porto.com>
2025-06-21 19:27:14 +0100

    Added briefs to functions

--- cpp04/ex01/Brain.cpp
+++ cpp04/ex01/Brain.cpp
@@ -6,17 +6,21 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 15:38:41 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/21 17:49:35 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/21 19:25:22 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
+/// @brief Default constructor. Initializes a Brain object.
 Brain::Brain(void)
 {
 	std::cout << "Brain default constructor called" << std::endl;
 }
 
+/// @brief Copy constructor.
+/// Copies all ideas from the source Brain.
+/// @param src The Brain object to copy from.
 Brain::Brain(Brain const& src)
 {
 	std::cout << "Brain copy constructor called" << std::endl;
@@ -24,6 +28,10 @@ Brain::Brain(Brain const& src)
 		this->ideas[i] = src.ideas[i];
 }
 
+/// @brief Copy assignment operator.
+/// Copies all ideas from the right-hand side Brain.
+/// @param rhs The Brain object to assign from.
+/// @return Reference to this Brain.
 Brain&	Brain::operator=(Brain const& rhs)
 {
 	if (this != &rhs)
@@ -35,11 +43,15 @@ Brain&	Brain::operator=(Brain const& rhs)
 	return (*this);
 }
 
+/// @brief Destructor.
 Brain::~Brain(void)
 {
 	std::cout << "Brain destructor called" << std::endl;
 }
 
+/// @brief Returns the idea stored at the given index.
+/// @param index The index of the idea to retrieve (0-99).
+/// @return The idea string if index is valid, otherwise an error message.
 std::string	Brain::getIdea(int index) const
 {
 	if (index >= 0 && index < 100)
@@ -48,6 +60,10 @@ std::string	Brain::getIdea(int index) const
 		return ("Invalid index to get idea");
 }
 
+/// @brief Sets the idea at the given index.
+/// @param index The index to set the idea at (0-99).
+/// @param idea The idea string to store.
+/// @note Prints an error message if index is invalid.
 void	Brain::setIdea(int index, std::string const& idea)
 {
 	if (index >= 0 && index < 100)
--- cpp04/ex01/Dog.cpp
+++ cpp04/ex01/Dog.cpp
@@ -6,24 +6,30 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 22:44:58 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/21 18:44:13 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/21 19:17:21 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 
+/// @brief Default constructor for Dog. Initializes type and alloDoges new Brain.
 Dog::Dog(void) : Animal(), dogBrain(new Brain())
 {
 	this->type = "Dog";
 	std::cout << "Dog default constructor called" << std::endl;
 }
 
+/// @brief Copy constructor for Dog. Performs deep copy of Brain.
+/// @param src The Dog object to copy from.
 Dog::Dog(Dog const& src) : Animal(src)
 {
 	this->dogBrain = new Brain(*src.dogBrain); //deep copy
 	std::cout << "Dog copy constructor called" << std::endl;
 }
 
+/// @brief Copy assignment operator for Dog. Deletes old Brain and deep copies new Brain.
+/// @param rhs The Dog object to assign from.
+/// @return Reference to this Dog object.
 Dog&	Dog::operator=(Dog const& rhs)
 {
 	if (this != &rhs)
@@ -37,27 +43,37 @@ Dog&	Dog::operator=(Dog const& rhs)
 	return (*this);
 }
 
+/// @brief Destructor for Dog. Deletes Brain to prevent memory leaks.
 Dog::~Dog(void)
 {
 	std::cout << "Dog destructor called" << std::endl;
 	delete(this->dogBrain);
 }
 
+/// @brief Makes the Dog sound.
 void	Dog::makeSound(void) const
 {
 	std::cout << this->type << " says: woof woof" << std::endl;
 }
 
+/// @brief Sets an idea in the Dog's Brain at the specified index.
+/// @param index The index to set the idea at.
+/// @param idea The idea string to store.
 void	Dog::setIdea(int index, std::string const& idea)
 {
 	this->dogBrain->setIdea(index, idea);
 }
 
+/// @brief Retrieves an idea from the Dog's Brain at the specified index.
+/// @param index The index to retrieve the idea from.
+/// @return The idea string stored at the index.
 std::string	Dog::getIdea(int index) const
 {
 	return (this->dogBrain->getIdea(index));
 }
 
+/// @brief Returns a pointer to the Dog's Brain.
+/// @return Const pointer to the Brain object.
 Brain const*	Dog::getBrainPtr(void) const
 {
 	return (this->dogBrain);
