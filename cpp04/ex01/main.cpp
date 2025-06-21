372ff17484ceffc45b7138feaec1162f185c8572
Mariana Morais <mariaoli@student.42porto.com>
2025-06-21 19:08:26 +0100

    Added more tests to main

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 21:01:51 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/21 09:57:34 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/21 18:57:10 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,47 +18,64 @@
 
 int	main(void)
 {
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	delete j; //should not create a leak
-	delete i;
-	//...
+	std::cout << "\n----Basic test----" << std::endl;
+	Animal const* d = new Dog();
+	Animal const* c = new Cat();
+
+	std::cout << "\n----Destruct heap allocated objects----" << std::endl;
+	delete(d);
+	delete(c);
+
+	std::cout << "\n----Array test----" << std::endl;
+	int const	arraySize = 4;
+	Animal const* animal[arraySize];
+
+	for (int i = 0; i < arraySize; i++)
+	{
+		if (i < arraySize / 2)
+		{
+			std::cout << i << std::endl;
+			animal[i] = new Dog();
+		}
+		else
+		{
+			std::cout << i << std::endl;
+			animal[i] = new Cat();
+		}
+	}
+
+	std::cout << "\n----Make sound test----" << std::endl;
+	for (int i = 0; i < arraySize; i++)
+	{
+		std::cout << i << std::endl;
+		animal[i]->makeSound();
+	}
+
+	std::cout << "\n----Destruct array----" << std::endl;
+	for (int i = 0; i < arraySize; i++)
+	{
+		std::cout << i << std::endl;
+		delete(animal[i]);
+	}
+
+	std::cout << "\n----Deep copy test----" << std::endl;
+	Cat	grumpy;
+	grumpy.setIdea(0, "Frown at people");
+	Cat	grumpyCopy(grumpy);
+	std::cout << "***Original idea: " << grumpy.getIdea(0) << " -> " << grumpy.getBrainPtr() << std::endl;
+	std::cout << "***Copy idea: " << grumpyCopy.getIdea(0) << " -> " << grumpyCopy.getBrainPtr() << std::endl;
+	grumpyCopy.setIdea(0, "Hiss at people");
+	std::cout << "***Modified copy idea: " << grumpyCopy.getIdea(0) << std::endl;
+	std::cout << "***Original idea: " << grumpy.getIdea(0) << std::endl;
+
+	std::cout << "\n----Busy mind test----" << std::endl;
+	Dog	milo;
+	for (int i = 0; i < 100; i++)
+		milo.setIdea(i, "Squirrel!");
+	for(int i = 0; i < 100; i++)
+		std::cout << "Milo's idea: " << milo.getIdea(i) << std::endl;
+
+	std::cout << "\n----Destruct stack-allocated objects----" << std::endl;
+
 	return (0);
 }
-
-// int	main(void) 
-// {
-// 	std::cout << "\n----Animal, Dog and Cat construct----" << std::endl;
-// 	Animal const*		meta = new Animal();
-// 	Animal const*		d = new Dog();
-// 	Animal const*		c = new Cat();
-
-// 	std::cout << "\n----Animal, Dog and Cat test----" << std::endl;
-// 	std::cout << "Animal is a: " << meta->getType() << " " << std::endl;
-// 	std::cout << "Dog is a: " << d->getType() << " " << std::endl;
-// 	std::cout << "Cat is a: " << c->getType() << " " << std::endl;
-// 	meta->makeSound();
-// 	d->makeSound(); 
-// 	c->makeSound();
-
-// 	std::cout << "\n----Animal, Dog and Cat destruct----" << std::endl;
-// 	delete(meta);
-// 	delete(d);
-// 	delete(c);
-	
-// 	std::cout << "\n----Wrong Animal and Wrong Cat construct----" << std::endl;
-// 	WrongAnimal const*	wrongMeta = new WrongAnimal();
-// 	WrongAnimal const*	wrongC = new WrongCat();
-
-// 	std::cout << "\n----Wrong Animal and Wrong Cat test----" << std::endl;
-// 	std::cout << "WrongAnimal is a: " << wrongMeta->getType() << " " << std::endl;
-// 	std::cout << "WrongCat is a: " << wrongC->getType() << " " << std::endl;
-// 	wrongMeta->makeSound();
-// 	wrongC->makeSound();
-
-// 	std::cout << "\n----Wrong Animal and Wrong Cat destruct----" << std::endl;
-// 	delete(wrongMeta);
-// 	delete(wrongC);
-
-// 	return (0);
-// }
