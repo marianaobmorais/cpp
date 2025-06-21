d9cd6d31bcd453760ecba0375cb7209cb850514a
Mariana <mariana.obm@gmail.com>
2025-06-21 09:29:10 +0100

    Turned makeSound() into virtual

--- cpp04/ex00/Animal.hpp
+++ cpp04/ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 18:07:19 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/20 22:29:03 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/06/20 23:52:26 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,11 @@ class Animal
 		std::string	type;
 	public:
 		Animal(void);
-		Animal(std::string &str);
 		Animal(Animal const& src);
 		Animal&	operator=(Animal const& rhs);
 		~Animal(void);
 
-		void		makeSound(void);
+		virtual void	makeSound(void) const;
 		std::string	getType(void) const;
 };
 
