b6ab5c13473d1d4c78c7f736ee1f762ef5c416ee
Mariana <mariana.obm@gmail.com>
2025-06-22 11:05:01 +0100

    Added IMateriaSource.hpp

---
+++ cpp04/ex03/IMateriaSource.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/22 11:02:30 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/06/22 11:03:51 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
+
+#include <string>
+#include "AMateria.hpp"
+
+class IMateriaSource
+{
+	public:
+		virtual ~IMateriaSource() {}
+		virtual void learnMateria(AMateria*) = 0;
+		virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+#endif //IMATERIASOURCE_HPP
