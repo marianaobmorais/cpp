32ce7407cf1fbdfc94599e76b218cb40bd08d3f7
Mariana <mariana.obm@gmail.com>
2025-06-22 11:05:26 +0100

    Added ICharacter.hpp

---
+++ cpp04/ex03/ICharacter.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/22 10:59:31 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/06/22 11:00:58 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+
+#include "AMateria.hpp"
+#include <string>
+
+class ICharacter
+{
+	public:
+		virtual ~ICharacter() {}
+		virtual std::string const & getName() const = 0;
+		virtual void equip(AMateria* m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter& target) = 0;
+};
+
+#endif //ICHARACTER_HPP
