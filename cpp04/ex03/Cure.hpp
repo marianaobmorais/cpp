7da758e78bf80ac9ced01576ef7c68f534088b7b
Mariana Morais <mariaoli@student.42porto.com>
2025-07-03 15:56:09 +0100

    Added Cure.hpp

---
+++ cpp04/ex03/Cure.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cure.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 13:53:17 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/07/03 15:50:08 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CURE_HPP
+# define CURE_HPP
+
+#include "AMateria.hpp"
+#include <string>
+
+class Cure : public AMateria
+{
+	public:
+		Cure(void);
+		Cure(Cure const& src);
+		Cure&	operator=(Cure const& rhs);
+		~Cure(void);
+
+		AMateria*	clone() const; //override
+		void		use(ICharacter& target);
+};
+
+#endif //CURE_HPP
