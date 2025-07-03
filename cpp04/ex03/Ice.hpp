2c3d6f0bc933d7ceb42935302b1f58b7cf4218b3
Mariana Morais <mariaoli@student.42porto.com>
2025-07-03 15:56:31 +0100

    Added Ice.hpp

---
+++ cpp04/ex03/Ice.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Ice.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 12:25:24 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/07/03 15:50:04 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICE_HPP
+# define ICE_HPP
+
+#include "AMateria.hpp"
+#include <string>
+
+class Ice : public AMateria
+{
+	public:
+		Ice(void);
+		Ice(Ice const& src);
+		Ice&	operator=(Ice const& rhs);
+		~Ice(void);
+
+		AMateria*	clone() const; //override
+		void		use(ICharacter& target); //override
+};
+
+#endif //ICE_HPP
