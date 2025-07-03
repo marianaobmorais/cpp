1fb272c50a5d6e2fd58cd9fcc206214609565715
Mariana Morais <mariaoli@student.42porto.com>
2025-07-03 19:11:50 +0100

    AMateria destructor is now virtual

--- cpp04/ex03/AMateria.hpp
+++ cpp04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 10:56:25 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/07/03 15:48:41 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/07/03 19:00:29 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,10 @@ class AMateria
 		AMateria(std::string const& type);
 		AMateria(AMateria const& src);
 		AMateria&	operator=(AMateria const& rhs);
-		~AMateria(void);
+		virtual ~AMateria(void);
 
 		std::string const&	getType() const;
+
 		virtual AMateria*	clone() const = 0; //clone is pure virtual because every derived class must implement how to clone itself
 		virtual void		use(ICharacter& target); //use has a default behavior, but can be overrinden if needed
 };
