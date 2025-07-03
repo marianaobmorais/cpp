271c6c70a457d3deb6b3afe2e6b40a71702dc71a
Mariana Morais <mariaoli@student.42porto.com>
2025-07-03 17:11:08 +0100

    Added missing members for AMateria class

--- cpp04/ex03/AMateria.hpp
+++ cpp04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 10:56:25 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/06/22 10:58:41 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/07/03 15:48:41 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,17 @@
 class AMateria
 {
 	protected:
-		//[...]
-    public:
+		std::string	type;
+	public:
+		AMateria(void);
 		AMateria(std::string const& type);
-		//[...]
-		std::string const&	getType() const; //Returns the materia type
-		virtual AMateria*	clone() const = 0;
-		virtual void		use(ICharacter& target);
+		AMateria(AMateria const& src);
+		AMateria&	operator=(AMateria const& rhs);
+		~AMateria(void);
+
+		std::string const&	getType() const;
+		virtual AMateria*	clone() const = 0; //clone is pure virtual because every derived class must implement how to clone itself
+		virtual void		use(ICharacter& target); //use has a default behavior, but can be overrinden if needed
 };
 
 #endif //AMATERIA_HPP
