f581a879db9f0a7143696de97a295643a82ca240
Mariana <mariana.obm@gmail.com>
2025-07-07 15:02:48 +0100

    Added more tests

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 14:24:10 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/07/07 14:19:10 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/07/07 15:00:35 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 #include "Character.hpp"
 #include "Cure.hpp"
 #include "Ice.hpp"
+#include <iostream>
 
 int main(void)
 {
-	IMateriaSource* src = new MateriaSource();
+	std::cout << "\n--------Basic tests--------" << std::endl;
+	IMateriaSource*	src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
-	ICharacter* me = new Character("me");
-	AMateria* tmp;
+	ICharacter*	me = new Character("me");
+	AMateria*	tmp;
 
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
@@ -31,8 +33,38 @@ int main(void)
 	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
+
+	std::cout << "\n--------Destruct--------" << std::endl;
 	delete bob;
 	delete me;
 	delete src;
+
+	std::cout << "\n--------Materia learning overflow--------" << std::endl;
+	IMateriaSource* src2 = new MateriaSource();
+	src2->learnMateria(new Ice());
+	src2->learnMateria(new Ice());
+	src2->learnMateria(new Cure());
+	src2->learnMateria(new Cure());
+	src2->learnMateria(new Cure());
+
+	std::cout << "\n--------Materia equipping tests--------" << std::endl;
+	ICharacter*	me2 = new Character("me2");
+	AMateria*	tmp2;
+	tmp2 = src2->createMateria("invalid");
+	me2->equip(tmp2);
+	tmp2 = src2->createMateria("ice");
+	me2->equip(tmp2);
+	tmp2 = src2->createMateria("ice");
+	me2->equip(tmp2);
+	tmp2 = src2->createMateria("cure");
+	me2->equip(tmp2);
+	tmp2 = src2->createMateria("cure");
+	me2->equip(tmp2);
+	tmp2 = src2->createMateria("ice");
+	me2->equip(tmp2);
+
+	std::cout << "\n--------Destruct--------" << std::endl;
+	delete me2;
+	delete src2;
 	return (0);
 }
