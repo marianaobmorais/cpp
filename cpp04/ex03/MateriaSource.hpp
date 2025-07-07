4e6f1f1f7dfc14664381acfdc3ceac5b600c797b
Mariana <mariana.obm@gmail.com>
2025-07-07 14:19:43 +0100

    Fixed typo, added parameter name and macro for better readability

--- cpp04/ex03/MateriaSource.hpp
+++ cpp04/ex03/MateriaSource.hpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 17:13:18 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/07/03 17:13:20 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/07/07 13:07:22 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 
 #include "IMateriaSource.hpp"
 
+#define MAX_TMPLT 4
+
 class MateriaSource : public IMateriaSource
 {
 	private:
-		AMateria*	templates[4];
+		AMateria*	templates[MAX_TMPLT];
 	public:
 		MateriaSource(void);
-		MateriaSource(MateriaSource const& scr);
+		MateriaSource(MateriaSource const& src);
 		MateriaSource&	operator=(MateriaSource const& rhs);
 		~MateriaSource(void);
 
-		void		learnMateria(AMateria*);
+		void		learnMateria(AMateria* m);
 		AMateria*	createMateria(std::string const & type);
 };
 
