f603e8c425345d6ba6420ac285c0a43cebbdd4f1
Mariana <mariana.obm@gmail.com>
2025-07-20 17:28:04 +0100

    Added files from ex00

---
+++ cpp05/ex01/main.cpp
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 15:56:51 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/07/20 17:17:03 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int	main(void)
+{
+	std::cout << "------------------Valid bureaucrat-------------------" << std::endl;
+	try
+	{
+		Bureaucrat	a("Arnold", 130);
+		std::cout << a;
+		std::cout << "After grade increment:\n";
+		a.increment();
+		std::cout << a;
+
+		std::cout << "After grade decrement:\n";
+		a.decrement();
+		std::cout << a;
+	}
+	catch(std::exception const& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	std::cout << "----------Invalid bureaucrat (grade too low)----------" << std::endl;
+	try
+	{
+		Bureaucrat	b("Bob", 151);
+		std::cout << b;
+	}
+	catch (std::exception const& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	std::cout << "----------Invalid bureaucrat (grade too high)----------" << std::endl;
+	try
+	{
+		Bureaucrat c("Carl", 0);
+		std::cout << c;
+	}
+	catch(std::exception const& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	std::cout << "----------------Copy assignment tests-----------------" << std::endl;
+	std::cout << "--Bureaucrats have same name--" << std::endl;
+	try
+	{
+		Bureaucrat	d("Denis", 50);
+		Bureaucrat	e("Denis", 100);
+		std::cout << "-Before assignment:\n" << d << e;
+
+		d = e;
+		std::cout << "-After assignment:\n" << d << e;
+	}
+	catch (std::exception const& e)
+	{
+		std::cout << "Exception: " << e.what() <<std::endl;
+	}
+
+	std::cout << "--Bureaucrats have different names-- " << std::endl;
+	try
+	{
+		Bureaucrat	f("Frank", 120);
+		Bureaucrat	g("Gordon", 30);
+		std::cout << "-Before assignment:\n" << f << g;
+
+		f = g;
+		std::cout << "-After assignment:\n" << f << g;
+	}
+	catch (std::exception const& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	std::cout << "------------------Increment overflow-------------------" << std::endl;
+	try
+	{
+		Bureaucrat	h("Hannah", 1);
+		std::cout << "-Before increment:\n" << h;
+		h.increment();
+		std::cout << "After increment:\n" << h;
+	}
+	catch (std::exception const& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	std::cout << "------------------Decrement overflow-------------------" << std::endl;
+	try
+	{
+		Bureaucrat	i("Igor", 150);
+		std::cout << "-Before decrement:\n" << i;
+		i.decrement();
+		std::cout << "After decrement:\n" << i;
+	}
+	catch (std::exception const& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+	return (0);
+}
