097b7fc7823fd1490f7b8f2086a26a0ca4f62bfb
Mariana <mariana.obm@gmail.com>
2025-07-22 09:50:09 +0100

    Added more tests to main

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 15:56:51 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/07/21 20:14:52 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/07/22 09:49:19 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 
 int	main(void)
 {
+	std::cout << "-----------------Shrubbery Creation Form-----------------"<< std::endl;
 	Bureaucrat	bob("Bob", 25);
 	ShrubberyCreationForm	home("home");
 	std::cout << bob;
@@ -26,6 +27,7 @@ int	main(void)
 	std::cout << home;
 	bob.executeForm(home);
 
+	std::cout << "-----------------Presidential Pardon Form-----------------"<< std::endl;
 	PresidentialPardonForm pardon("Arthur");
 	std::cout << pardon;
 	bob.executeForm(pardon);
@@ -34,55 +36,19 @@ int	main(void)
 	bob.executeForm(pardon);
 
 	Bureaucrat	billy("Billy", 2);
+	std::cout << billy;
 	billy.signForm(pardon);
 	std::cout << pardon;
 	billy.executeForm(pardon);
 	
-	// std::cout << "-----------------Valid form-----------------"<< std::endl;
-	// try
-	// {
-	// 	Form f("Some Form", 1, 1);
-	// 	std::cout << f;
-	// 	std::cout << "--Lower bureaucrat grade--"<< std::endl;
-	// 	Bureaucrat b("Bob", 4);
-	// 	std::cout << b;
-	// 	b.signForm(f);
-	// 	std::cout << f;
-	// 	std::cout << "--Valid bureaucrat grade--"<< std::endl;
-	// 	Bureaucrat b2("Beatrice", 1);
-	// 	std::cout << b2;
-	// 	b2.signForm(f);
-	// 	std::cout << f;
-	// 	std::cout << "--Invalid sign--"<< std::endl;
-	// 	Bureaucrat b3("Bruno", 1);
-	// 	std::cout << b3;
-	// 	b3.signForm(f);
-	// }
-	// catch (std::exception const& e)
-	// {
-	// 	std::cout << "Exception: " << e.what() << std::endl;
-	// }
+	std::cout << "-----------------Robotomy Request Form-----------------"<< std::endl;
+	AForm* formPtr = new RobotomyRequestForm("Marvin");
+	std::cout << *formPtr;
+	billy.signForm(*formPtr);
+	std::cout << *formPtr;
+	formPtr->execute(billy);
 
-	// std::cout << "-----------------Invalid form-----------------"<< std::endl;
-	// std::cout << "--Too high grade to sign--"<< std::endl;
-	// try
-	// {
-	// 	Form f2("Invalid form", 0, 150);
-	// 	std::cout << f2;
-	// }
-	// catch (std::exception const& e)
-	// {
-	// 	std::cout << "Exception: " << e.what() << std::endl;
-	// }
-	// std::cout << "--Too low grade to execute--"<< std::endl;
-	// try
-	// {
-	// 	Form f3("Invalid form", 15, 151);
-	// 	std::cout << f3;
-	// }
-	// catch (std::exception const& e)
-	// {
-	// 	std::cout << "Exception: " << e.what() << std::endl;
-	// }
-	// return (0);
+	delete formPtr;
+
+	return (0);
 }
