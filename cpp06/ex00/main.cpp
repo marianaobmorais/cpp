395d49ccaf81bee32127c273c90d83445e3183c6
Mariana Morais <mariaoli@student.42porto.com>
2025-07-29 19:30:17 +0100

    Commented out debug test

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:55:28 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/07/29 13:26:52 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/07/29 19:17:21 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,11 @@ int	main(int argc, char** argv)
 	}
 	std::string	str(argv[1]);
 
-	std::cout << "DEBUG: input str: [" << str << "], length: " << str.length() << "\n";
-	for (size_t i = 0; i < str.length(); ++i)
-	std::cout << "char[" << i << "] = '" << str[i] << "' (" << static_cast<int>(str[i]) << ")\n\n";
+	// DEBUG
+	// std::cout << "INPUT: str: [" << str << "], length: " << str.length() << "\n";
+	// for (size_t i = 0; i < str.length(); ++i)
+	// std::cout << "char[" << i << "] = '" << str[i] << "' (" << static_cast<int>(str[i]) << ")\n";
+	// std::cout << std::endl;
 
 	ScalarConverter::convert(str);
 	return (0);
