9b659c778b9fef0b6372ffc87e4e29a9439c22de
Mariana <mariana.obm@gmail.com>
2025-08-02 00:45:21 +0100

    Refactored generate() and identify(Base *p)

--- cpp06/ex02/Base.cpp
+++ cpp06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 12:41:31 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/08/02 00:26:01 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/08/02 00:44:37 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,41 +25,32 @@ Base*	generate(void)
 	int	n = std::rand() % 3;
 
 	std::cout << "[Generate]: ";
-	if (n == 0)
+	switch (n)
 	{
-		std::cout << "A" << std::endl;
-		Base*	res = new A();
-		return (res);
+		case 0:
+			std::cout << "A" << std::endl;
+			return (new A());
+		case 1:
+			std::cout << "B" << std::endl;
+			return (new B());
+		case 2:
+			std::cout << "C" << std::endl;
+			return (new C());
 	}
-	else if (n == 1)
-	{
-		std::cout << "B" << std::endl;
-		Base*	res = new B();
-		return (res);
-	}
-	else
-	{
-		std::cout << "C" << std::endl;
-		Base*	res = new C();
-		return (res);
-	}
+	return (NULL);
 }
 
 void	identify(Base* p)
 {
-	A*	a = dynamic_cast<A*>(p);
-	B*	b = dynamic_cast<B*>(p);
-	C*	c = dynamic_cast<C*>(p);
-
 	std::cout << "Actual type of the object pointed to by p: ";
-	if (a)
+	if (dynamic_cast<A*>(p))
 		std::cout << "A" << std::endl;
-	else if (b)
+	else if (dynamic_cast<B*>(p))
 		std::cout << "B" << std::endl;
-	else if (c)
+	else if (dynamic_cast<C*>(p))
 		std::cout << "C" << std::endl;
 	else
-		std::cout << "unkwon" << std::endl;
+		std::cout << "unknown" << std::endl;
 
 }
 
@@ -90,5 +81,5 @@ void	identify(Base& p)
 	}
 	catch (std::exception const& e) {}
 
-	std::cout << "unkwon" << std::endl;
+	std::cout << "unknown" << std::endl;
 }
