0b9f2e5892e71e9617ace60935cd298fdc81ceab
Mariana <mariana.obm@gmail.com>
2025-08-12 22:47:15 +0100

    Created Array.hpp

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 21:04:43 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/08/12 22:41:08 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+#include <stdexcept>
+
+template<typename T>
+class Array
+{
+	private:
+		T*				_elements;
+		unsigned int	_size;
+	public:
+		Array(void);
+		Array(unsigned int n);
+		Array(Array const& src);
+		Array&	operator=(Array const& rhs);
+		~Array();
+
+		T const&	operator[](unsigned int index) const;
+		T&	operator[](unsigned int index);
+
+		unsigned int	size(void) const;
+};
+
+#include "Array.tpp"
+
+#endif //ARRAY_HPP
